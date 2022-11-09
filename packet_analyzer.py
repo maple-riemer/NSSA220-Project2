3636c31788e82ca7cb927d31e98ce038f5a904e2
nar9394 <nar9394@rit.edu>
2022-11-09 09:39:23 -0500

    Set up packet_analyzer starter code

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -1,3 +1,4 @@
+import sys
 from filter_packets import *
 from packet_parser import *
 from compute_metrics import *
@@ -5,3 +6,10 @@ from compute_metrics import *
 filter()
 parse()
 compute()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 1:
+        print("Usage packet_analyzer.py")
+        sys.exit()
+    else:
+        print("Placeholder Text")
