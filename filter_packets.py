79af0b3a5cecd5d097172c786d1e5ed4157fa226
nar9394 <nar9394@rit.edu>
2022-11-17 14:42:12 -0500

    Implemented filter_packets.py

--- filter_packets.py
+++ filter_packets.py
@@ -1,3 +1,22 @@
-def filter() :
-	print('called filter function in filter_packets.py')
-	
+import re
+
+
+def filter(filename):
+    f = open(filename)
+    packets = []
+    adding = False
+    checking = False
+    for line in f:
+        if not checking and adding and not line.startswith("No.") and line != '\n':
+            packets[-1].append(re.sub(r' +', ' ', line).strip().split(' '))
+        if checking:
+            cleanLine = re.sub(r' +', ' ', line).strip().split(' ')
+            if "ICMP" in cleanLine and "Echo" in cleanLine:
+                adding = True
+                packets.append([])
+                packets[-1].append(cleanLine)
+            checking = False
+        if line.startswith("No."):
+            adding = False
+            checking = True
+    return packets
--- packet_analyzer.py
+++ packet_analyzer.py
@@ -3,13 +3,10 @@ from filter_packets import *
 from packet_parser import *
 from compute_metrics import *
 
-filter()
-parse()
-compute()
-
 if __name__ == "__main__":
     if len(sys.argv) != 1:
         print("Usage packet_analyzer.py")
         sys.exit()
     else:
-        print("Placeholder Text")
+        packets = filter("data/Node1.txt")
+        print(packets[0])
