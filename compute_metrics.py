5aca34c88c65ae58bb579c1cb1fb37774039fbd5
jillianebling <jee6650@g.rit.edu>
2022-11-20 19:25:09 -0500

    fixed search function

--- compute_metrics.py
+++ compute_metrics.py
@@ -6,15 +6,12 @@ def compute() :
 	#print(search("data/Node1.txt","reply"))
 
 #Something slightly off here
-#skipping lines; when told to print every line, it skips a considerable amount of them
-# this is why not everything is being counted correctly
-# thinking it has something to do with the try/except?? I have no idea... 
 def search(filename, string):
 	f = open(filename, "r")
 	count = 0
 	for line in f:
 		try:
-			line = f.readline().strip().split()
+			line = line.strip().split()
 			if (line[2] == IP_NODE_1 and line[8] == string):
 				print(line)
 				count += 1
