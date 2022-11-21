216424a824887e4b4d1559db0049bc3d5c0eddbe
jillianebling <jee6650@g.rit.edu>
2022-11-20 21:16:54 -0500

    data metrics almost done

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,23 +1,51 @@
 IP_NODE_1 = "192.168.100.1"
 
-def compute() :
-	print('called compute function in compute_metrics.py')
-	print(search("data/Node1.txt", "request"))
-	#print(search("data/Node1.txt","reply"))
+def compute():
+	print(search("data/Node1.txt",IP_NODE_1))
 
-#Something slightly off here
-def search(filename, string):
+def search(filename,ip):
+	#messy, inefficent. Will be organized soon.
 	f = open(filename, "r")
-	count = 0
+
+	ReqSentCount = 0
+	ReqRecieveCount = 0
+	RepSentCount = 0
+	RepRecieveCount = 0
+
+	ReqSentBytes = 0
+	ReqRecieveBytes = 0
+	RepSentBytes = 0 #not implemented
+	RepRecieveBytes = 0 #not implemented
+
 	for line in f:
 		try:
 			line = line.strip().split()
-			if (line[2] == IP_NODE_1 and line[8] == string):
-				print(line)
-				count += 1
+
+			#Computing Data Metrics...
+			if (line[2] == ip and line[8] == "request"):
+				ReqSentCount += 1 #data metric 1
+				ReqSentBytes += int(line[5]) #data metric 5
+
+			elif (line[3] == ip and line[8] == "request"):
+				ReqRecieveCount += 1 #data metric 2
+				ReqRecieveBytes += int(line[5]) #data metric 6
+
+			elif (line[2] == ip and line[8] == "reply"):
+				RepSentCount += 1 #data metric 3
+				#add data metric for #7
+
+			elif (line[3] == ip and line[8] == "reply"):
+				RepRecieveCount += 1 #data metric 4
+				#add data metric for #8
+
 		except IndexError:
 			continue
+
 	f.close()
-	return count
+
+	DataCountMetrics=[ReqSentCount,ReqRecieveCount,RepSentCount,RepRecieveCount]
+	DataByteMetrics=[ReqSentBytes,ReqRecieveBytes,RepSentBytes,RepRecieveBytes]
+
+	return DataCountMetrics,DataByteMetrics
 
 compute()
