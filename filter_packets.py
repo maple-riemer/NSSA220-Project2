e66d8afc7cdf4b2532bcde531f8effc77532edf6
nar9394 <nar9394@rit.edu>
2022-12-01 14:59:48 -0500

    Changes filter_packets.py output to include headers and spacing lines in it's output

--- filter_packets.py
+++ filter_packets.py
@@ -4,20 +4,20 @@ import re
 def filter(filename):
     f = open(filename, "r")
     output = open(filename.replace(".txt", "_filtered.txt"), "w")
+    output.write("No. Time Source Destination Protocol Length Info\n")
     packets = []
     adding = False
     checking = False
     for line in f:
         clean_line = re.sub(r' +', ' ', line).strip() + "\n"
-        if not checking and adding and not line.startswith("No.") and line != '\n':
+        if not checking and adding:
             output.write(clean_line)
         if checking:
+            checking = False
             if "ICMP" in clean_line and "Echo" in clean_line:
                 adding = True
                 output.write(clean_line)
-            checking = False
         if line.startswith("No."):
             adding = False
             checking = True
-    
     f.close()
